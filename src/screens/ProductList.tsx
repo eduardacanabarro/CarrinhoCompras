776c45c797d94272ba683cf03d9046ff32837b28
Henrique Varga <119634135+VargaHenrique@users.noreply.github.com>
2024-07-04 20:16:33 -0300

    Salvando arquivos para a pasta CarrinhoCompras

---
+++ src/screens/ProductList.tsx
@@ -0,0 +1,20 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const ProductList = () => {
+  return (
+    <View>
+      <Text style={styles.title}>ProductList</Text>
+    </View>
+  );
+};
+
+export default ProductList;
+
+const styles = StyleSheet.create({
+
+title:{
+  color: "#FF00FF"
+}
+
+});
