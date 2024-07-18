fe6da302e48ca6aa17da06e35fd99795481befe0
Henrique Varga <119634135+VargaHenrique@users.noreply.github.com>
2024-07-18 14:50:57 -0300

    Editando o ProductDetails

--- src/screens/ProductDetails.tsx
+++ src/screens/ProductDetails.tsx
@@ -1,10 +1,30 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity, FlatList, Image } from "react-native";
 import React from "react";
+import { ProductDTO } from "../types/Product"; 
 
 
 const ProductDetails = () => {
-  return (
+  
+  const renderProduct = ({ item }: { item: ProductDTO }) => (
+    <View style={styles.productContainer}>
+      <View style={styles.productInfo}>
+        <Image source={{ uri: item.image }} style={styles.productImage} />
+      </View>
+      <View style={styles.productDetails}>
+        <Text style={styles.productTitle}>{item.title}</Text>
+        <Text style={styles.productPrice}>${item.price}</Text>
+        <Text style={styles.productCategory}>${item.category}</Text>
+        <Text style={styles.producDescription}>${item.description}</Text>
+        <Text style={styles.productRating}>{item.rating.count} - {item.rating.rate}</Text>
+      </View>
+    </View>
+  );
+  
+  return (  
     <View>
+      
+      
+      
       <Text>ProductDetails</Text>
     </View>
   );
@@ -13,6 +33,32 @@ const ProductDetails = () => {
 export default ProductDetails;
 
 const styles = StyleSheet.create({
+  productContainer:{
 
+  }, 
+  productInfo: {
+  
+  },
+  productImage:{
 
+  },
+  productTitle:{
+
+  }, 
+  productPrice:{
+
+  },
+  productDetails:{
+
+  },
+  productCategory:{
+
+  },
+  producDescription:{
+
+  },
+  productRating:{
+
+  },     
+ 
 });
